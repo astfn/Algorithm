f3e14fec0d4e25c1403cdfd89695eaa6d9609af1
astfn <876458652@qq.com>
2022-01-01 16:01:10 +0800

    update Algorithm documents

--- 03-LeetCode/01-初级算法/05-排序和搜索/01-合并两个有序数组/01-代码实现/index.js
+++ 03-LeetCode/01-初级算法/05-排序和搜索/01-合并两个有序数组/01-代码实现/index.js
@@ -1,43 +1,20 @@
-/* 方案一 */
-//双指针遍历两个Array
-//找到nums2的当前元素，在nums1中的合适位置，进行插入即可，具体步骤如下
-// 如果 nums2[p2]<=nums1[p1] ,让nums1[p1]及其之后的元素依次向后移动，再让nums2[p2]放置到nums1[p1]处。
-
 export default function merge(nums1, m, nums2, n) {
-  if (m === 0) {
-    nums2.map((v, i) => (nums1[i] = v));
-    return nums1;
-  }
-
   let p1 = 0;
   let p2 = 0;
 
-  while (p1 < m && p2 < n) {
+  while (p1 < m + p2 && p2 < n) {
     if (nums2[p2] <= nums1[p1]) {
-      for (let i = nums1.length - 1; i > p1; i--) {
+      for (let i = m + p2; i > p1; i--) {
         nums1[i] = nums1[i - 1];
       }
-      nums1[p1 + 1] = nums1[p1];
       nums1[p1] = nums2[p2];
       p2++;
-      p1++;
-      m++;
-    } else {
-      p1++;
     }
-  }
-
-  while (p2 < n) {
-    nums1[p1] = nums2[p2];
     p1++;
+  }
+  while (p2 < n) {
+    nums1[m + p2] = nums2[p2];
     p2++;
   }
-
   return nums1;
 }
-
-// export default function merge(nums1, m, nums2, n) {
-//   nums1.length = m;
-//   nums2.map((v, i) => nums1.push(v));
-//   return (nums1 = nums1.sort((a, b) => a - b));
-// }
